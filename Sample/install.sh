232039e5b3b8f33326f3ca42ee0cacf724a6ebd8
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2018-09-24 11:54:10 -0300

    Update samples

--- Sample/install.sh
+++ Sample/install.sh
@@ -2,6 +2,6 @@
 
 rm -rf ../node_modules/
 rm -rf node_modules/
-rm yarn.lock
 yarn install
 rm -rf node_modules/react-native-keyboard-manager/Sample/
+rm -rf node_modules/react-native-keyboard-manager/SamplePod/
